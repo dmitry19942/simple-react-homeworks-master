e66241df9ddc99b974a54716800c8d4dabe89a43
dmitry19942 <gorustovich2013@gmail.com>
2022-09-28 21:42:47 +0300

    Updates

---
+++ precache-manifest.0c70c607b1ec12fbc57705969246eeef.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "04d34acb415339c12c5a",
+    "url": "/simple-react-homeworks-master/static/css/main.58ed6f23.chunk.css"
+  },
+  {
+    "revision": "04d34acb415339c12c5a",
+    "url": "/simple-react-homeworks-master/static/js/main.96286106.chunk.js"
+  },
+  {
+    "revision": "934caa94c485bae7831b",
+    "url": "/simple-react-homeworks-master/static/js/runtime~main.0ffd70ec.js"
+  },
+  {
+    "revision": "89931354b70e204b0e08",
+    "url": "/simple-react-homeworks-master/static/js/2.6c0280be.chunk.js"
+  },
+  {
+    "revision": "b89439a385941c48e82cb3b0f7e9db40",
+    "url": "/simple-react-homeworks-master/index.html"
+  }
+];
